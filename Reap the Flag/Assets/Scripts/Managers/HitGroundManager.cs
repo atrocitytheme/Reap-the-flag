edc549459efac184e3340f3485f8f12755c9e0c5
Junxuan Wu <atrocitytheme@gmail.com>
2019-07-09 18:04:58 +0800

    add ground effect

---
+++ Reap the Flag/Assets/Scripts/Managers/HitGroundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HitGroundManager : MonoBehaviour, Damagable
+{
+    public GameObject particleEffects;
+    List<GameObject> objects = new List<GameObject>();
+    // Start is called before the first frame update
+    public void TakeDamage(int amount, Vector3 hitPoint) {
+        GameObject particle = Instantiate(particleEffects, hitPoint, Quaternion.identity) as GameObject;
+        
+        particle.GetComponent<ParticleSystem>().Play();
+        objects.Add(particle);
+    }
+    // TODO: revise this to promise
+    private void Update()
+    {
+        objects.ForEach((d) => {
+            if (!d.GetComponent<ParticleSystem>().isPlaying) {
+                Destroy(d);
+            }
+        });
+        objects.RemoveAll((d) => {
+            return !d.GetComponent<ParticleSystem>().isPlaying;
+        });
+    }
+
+    public bool IsDead() {
+        return false;
+    }
+}
