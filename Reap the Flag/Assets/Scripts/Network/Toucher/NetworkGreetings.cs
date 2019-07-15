5e6a608d1451252ff844386af419cf4b79193583
Junxuan Wu <atrocitytheme@gmail.com>
2019-07-15 18:07:20 +0800

    add udp connection

--- Reap the Flag/Assets/Scripts/Network/Toucher/NetworkGreetings.cs
+++ Reap the Flag/Assets/Scripts/Network/Toucher/NetworkGreetings.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
-using Microsoft.AspNetCore.SignalR.Client;
+﻿using LiteNetLib;
+using UnityEngine;
 public class NetworkGreetings : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("start!");
+        EventBasedNetListener listener = new EventBasedNetListener();
+        NetManager client = new NetManager(listener);
+        client.Start();
+        client.Connect("10.7.8.185", 9956, "cut!");
+        listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+        {
+            Debug.Log("we got: {0} " + dataReader.GetString(100));
+        };
     }
 }
