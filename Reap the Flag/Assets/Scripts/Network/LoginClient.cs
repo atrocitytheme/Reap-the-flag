976ab974944863a272998b8543734af2b0164e4b
Junxuan Wu <atrocitytheme@gmail.com>
2019-07-24 13:04:52 +0800

    complete login status

--- Reap the Flag/Assets/Scripts/Network/LoginClient.cs
+++ Reap the Flag/Assets/Scripts/Network/LoginClient.cs
@@ -5,11 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoginClient : MonoBehaviour
 {
     public LoginPanelOperator panel;
     public string url;
+    public InputField id;
+    public InputField name;
+    public InputField password;
     HttpClient client = new HttpClient();
 
     private void Awake()
@@ -17,6 +21,7 @@ public class LoginClient : MonoBehaviour
         client.Timeout = TimeSpan.FromSeconds(1);
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
     }
+
     public void ConnecteToServer()
     {
         var isSuccess = Task.Run(() => Request()).Result;
@@ -30,13 +35,17 @@ public class LoginClient : MonoBehaviour
         panel.DisplayInfo(isSuccess);
         
     }
+    private string extractFromInput(InputField field) {
+        return field.text;
+    }
 
     private async Task<bool> Request() {
-
         try
         {
             var content = new FormUrlEncodedContent(new KeyValuePair<string, string>[]{
-                new KeyValuePair<string, string>("as", "ads")
+                new KeyValuePair<string, string>("Name", extractFromInput(name)),
+                new KeyValuePair<string, string>("Password", extractFromInput(password)),
+                new KeyValuePair<string, string>("Id", extractFromInput(id))
             });
             var msg = await client.PostAsync(url, content);
             msg.EnsureSuccessStatusCode();
---
+++ Reap the Flag/Assets/Scripts/Network/Model/FormModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string name;
+    private string id;
+    private string password;
+
+    public string Name {
+        get
+        {
+            return name;
+        }
+
+        set {
+            name = value;
+        }
+    }
+
+    public string Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Password
+    {
+        get
+        {
+            return password;
+        }
+
+        set
+        {
+            password = value;
+        }
+    }
+}
